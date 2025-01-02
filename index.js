61643c6fc73ebeb69118ab175a80f17e3b3e2578
shahidul shohid <shahidulislamshohid7gmail.com>
2025-01-02 19:26:18 +0600

    post and delete method implement done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wnw5g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -42,12 +42,20 @@ async function run() {
 
     //carts collection 
     app.get('/carts', async(req, res) => {
-      const result = await cartCollection.find().toArray()
+      const email = req.query.email
+      const query = {email:email}
+      const result = await cartCollection.find(query).toArray()
       res.send(result)
     })
     app.post('/carts', async(req, res) => {
       const cartItem = req.body
       const result = await cartCollection.insertOne(cartItem)
+      res.send(result)
+    })
+    app.delete('/carts/:id', async(req, res)=> {
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
       res.send(result)
     })
 
