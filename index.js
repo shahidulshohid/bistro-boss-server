d75c9ca6a9dcc382c16f2191857fdae71cb132b2
shahidul shohid <shahidulislamshohid7gmail.com>
2025-01-05 13:37:28 +0600

    userCollection create is done

--- index.js
+++ index.js
@@ -26,9 +26,17 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db('bistroDB').collection('users')
     const menuCollection = client.db('bistroDB').collection('menu')
     const reviewsCollection = client.db('bistroDB').collection('reviews')
     const cartCollection = client.db('bistroDB').collection('carts')
+
+    // user related api 
+    app.post('/users', async(req, res) => {
+      const user = req.body 
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
 
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray()
