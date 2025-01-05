7781f11218226d8f7fcd16b0630f653a0a80b167
shahidul shohid <shahidulislamshohid7gmail.com>
2025-01-05 16:42:37 +0600

    delete functionality is done

--- index.js
+++ index.js
@@ -32,12 +32,31 @@ async function run() {
     const cartCollection = client.db('bistroDB').collection('carts')
 
     // user related api 
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
     app.post('/users', async(req, res) => {
       const user = req.body 
+      //insert email if user doesn't exist
+      //you can do this many ways (1. email is unique, 2. upsert 3.simple checking email)
+      const query = {email:user.email}
+      const existingUser = await  userCollection.findOne(query)
+      if(existingUser) {
+        return res.send({message:"User already exists", insertedId:null})
+      }
       const result = await userCollection.insertOne(user)
       res.send(result)
     })
 
+    app.delete('/users/:id', async(req, res)=> {
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+    
+    //menu related apis
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray()
         res.send(result)
